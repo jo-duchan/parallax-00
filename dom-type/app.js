b39e3d01713b215899a0904b6ad1ac199bead1d0
jo-duchan <jo_duchan@icloud.com>
2022-12-15 18:50:36 +0900

    DOM Type 1차 완료

---
+++ dom-type/app.js
@@ -0,0 +1,42 @@
+let x = 0;
+let y = 0;
+let mx = 0;
+let my = 0;
+let speed = 0.009;
+let _imgArr;
+let bird;
+
+function init() {
+  _imgArr = document.getElementsByClassName("_imgArr");
+  bird = document.getElementsByClassName("layer-03")[0];
+  window.addEventListener("mousemove", handleMove, false);
+
+  function handleMove(e) {
+    x = e.clientX - window.innerWidth / 2;
+    y = e.clientY - window.innerHeight / 2;
+  }
+  loop();
+}
+
+function loop() {
+  mx += (x - mx) * speed;
+  my += (y - my) * speed;
+
+  bird.style.transform = `
+    translate3d(${(mx / 7) * -1}px, ${(my / 7) * -1}px, 0)
+  `;
+  _imgArr[1].style.transform = `
+    translate3d(${(mx / 10) * -1}px, ${(my / 10) * -1}px, 0)
+  `;
+  _imgArr[2].style.transform = `
+    translate3d(${(mx / 5) * -1}px, ${(my / 5) * -1}px, 0)
+  `;
+  _imgArr[3].style.transform = `
+    translate3d(${(mx / 3) * -1}px, ${(my / 3) * -1}px, 0)
+  `;
+  window.requestAnimationFrame(loop);
+}
+
+window.onload = function () {
+  init();
+};
